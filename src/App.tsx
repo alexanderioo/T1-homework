2bf59d2cc1d6c4c178e807e9e99cf51c15e8c25a
AlexFrolov <frolof022a@gmail.com>
2025-07-13 20:20:10 +0300

    adding Admiral UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import styled from "styled-components";
+import "./App.css";
+
+import reactLogo from "./assets/react.svg";
+import { T, Link } from "@admiral-ds/react-ui";
+
+const Divider = styled.div`
+  width: 10px;
+  height: 12px;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -18,18 +25,30 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="button"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
+        <T font="Subtitle/Subtitle 1" as="p">
           Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        </T>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Link
+        appearance="primary"
+        href="https://admiralds.github.io/react-ui"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Admiral Storybook
+        <Divider />
+      </Link>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
